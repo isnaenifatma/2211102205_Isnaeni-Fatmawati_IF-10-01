7537bc19cd20ec619cb608beb731fadc0f8c6025
isnaenifatmawati90@gmail.com <isnaenifatmawati90@gmail.com>
2025-03-07 13:40:03 +0700

    up

--- Modul3_isnaenifatmawati_2211102205/test/widget_test.dart
+++ Modul3_isnaenifatmawati_2211102205/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:modul3_isnaenifatmawati_2211102205/main.dart';
+import 'package:modul3_isnaenifatmawati_2211102205/widget.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
