fc6dca30c22ba158969522ddc7b30da2bce382e7
isnaenifatmawati90@gmail.com <isnaenifatmawati90@gmail.com>
2025-03-24 14:18:09 +0700

    up

--- tugas5_1_networking/android/app/src/main/kotlin/com/example/tugas5_1_networking/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tugas5_1_networking
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- tugas5_1_networking_comments/android/app/src/main/kotlin/com/example/tugas5_1_networking_comments/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tugas5_1_networking_comments
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
